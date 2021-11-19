73c763dc56f4361279b6d5697d04b7218795d426
Louis PAINTER <E211731S@univ-nantes.fr>
2021-11-19 15:12:29 +0100

    Solve doublons4

--- recherche/termine/doublons4/doublons4.go
+++ recherche/termine/doublons4/doublons4.go
@@ -1,5 +1,4 @@
 package doublons4
-
 /*
 On dispose d'un tableau d'entiers de longueur n et on suppose qu'il contient
 exactement k fois chaque nombre compris entre 1 et n/k. On voudrait vérifier
@@ -16,9 +15,31 @@ entiers de 1 à len(tab)/k
 */
 
 func doublons(tab []int) (k int, ok bool) {
-
 	if len(tab) == 0 || tab == nil {
 		return 0, true
 	}
+	var vus []int = make([]int, len(tab))
+
+	for i := 0; i < len(tab); i++ {
+		if tab[i] > len(tab) || tab[i] < 1 {
+			return 0, false
+		} 
+		vus[tab[i] - 1] += 1
+	}
+	k = vus[0]
+	var warning bool
+	for i := 0; i < len(vus); i++ {
+		if vus[i] == 0 {
+			if warning {
+				break
+			}
+			warning = true
+		}else {
+			if vus[i] != k || warning {
+				return 0, false
+			}
+		}
+	}
+	return k, true
 
 }
