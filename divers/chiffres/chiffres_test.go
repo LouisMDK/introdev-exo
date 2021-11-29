d6b9a857d5c360ee54bec6af4c890c4da794eee8
Louis PAINTER <E211731S@univ-nantes.fr>
2021-11-29 09:33:21 +0100

    add new tests

--- divers/chiffres/chiffres_test.go
+++ divers/chiffres/chiffres_test.go
@@ -1,9 +1,6 @@
 package chiffres
 
-import (
-	"testing"
-	"fmt"
-)
+import "testing"
 
 func Test0(t *testing.T) {
 	res := chiffres(0)
@@ -20,9 +17,16 @@ func TestPetit(t *testing.T) {
 }
 
 func TestGrand(t *testing.T) {
-	fmt.Println(chiffres(1001))
 	res := chiffres(10)
 	if res != 405071317 {
 		t.Error("chiffres(10) devrait retourner 405071317 mais retourne ", res)
+	}
+}
+
+// Ajouté suite au test machine 2021-2022
+func TestTresGrand(t *testing.T) {
+	res := chiffres(25)
+	if res != 2284082997 {
+		t.Fail()
 	}
 }
